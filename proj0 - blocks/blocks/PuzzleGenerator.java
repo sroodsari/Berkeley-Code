c3c7e971f7e64c827d51edf630462db8cf5eed89
Susan Roodsari <sroodsari@berkeley.edu>
2023-02-28 01:15:59 -0800

    61b project 0 blocks

---
+++ proj0 - blocks/blocks/PuzzleGenerator.java
@@ -0,0 +1,99 @@
+package blocks;
+
+import java.util.Random;
+
+import static blocks.Utils.*;
+
+/** A creator of random Blocks puzzles.
+ *  @author susanroodsari
+ */
+class PuzzleGenerator implements PuzzleSource {
+
+    /** A new PuzzleGenerator whose random-number source is seeded
+     *  with SEED. */
+    PuzzleGenerator(long seed) {
+        _random = new Random(seed);
+    }
+
+    /* By default, the convention is that overriding methods have the same
+     * comment as on the method they override. See PuzzleSource. */
+    @Override
+    public boolean deal(Model model, int handSize) {
+        assert handSize > 0;
+        model.clearHand();
+        int pieceLen = PIECES.length;
+        int nextPiece = _random.nextInt(pieceLen);
+        for (int h = 0; h < handSize; h++) {
+            Piece randPiece = PIECES[nextPiece];
+            model.deal(randPiece);
+        }
+
+
+        return true;
+    }
+
+    @Override
+    public void setSeed(long seed) {
+        _random.setSeed(seed);
+    }
+
+    /** My PNRG. */
+    private Random _random;
+
+    /** Pieces available to be dealt to hand. */
+    static final Piece[] PIECES = {
+        new Piece("*** *** ***"),
+        new Piece("** **"),
+        new Piece("*"),
+
+        new Piece("** ** **"),
+        new Piece("*** ***"),
+
+        new Piece("**"),
+        new Piece("* *"),
+        new Piece("***"),
+        new Piece("* * *"),
+
+        new Piece("** *."),
+        new Piece("** .*"),
+        new Piece("*. **"),
+        new Piece(".* **"),
+
+        new Piece("** *. **"),
+        new Piece("** .* **"),
+        new Piece("*.* ***"),
+        new Piece("*** *.*"),
+
+        new Piece("*.. *** *.."),
+        new Piece("..* *** ..*"),
+        new Piece(".*. .*. ***"),
+        new Piece("*** .*. .*."),
+
+        new Piece("*** ..* ..*"),
+        new Piece("..* ..* ***"),
+        new Piece("*** *.. *.."),
+        new Piece("*.. *.. ***"),
+
+        new Piece("** *. *."),
+        new Piece("** .* .*"),
+        new Piece("*.. ***"),
+        new Piece("*** *.."),
+
+        new Piece("*. .*"),
+        new Piece(".* *."),
+
+        new Piece("*.. .*. ..*"),
+        new Piece("..* .*. *.."),
+
+        new Piece("*.* .*. *.*"),
+        new Piece(".*. *.* .*."),
+
+        new Piece(".*. *** .*."),
+
+        new Piece(".** **."),
+        new Piece("**. .**"),
+        new Piece("*. ** .*"),
+        new Piece(".* ** *.")
+    };
+
+}
